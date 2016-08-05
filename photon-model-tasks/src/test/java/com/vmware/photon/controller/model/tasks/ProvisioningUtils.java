51d0486e4dd36f663ffdeba196f732d15a4d9a58
stonebai <sbai@vmware.com>
2016-08-05 13:56:40 -0700

    ProvisioningUtils
    Summary:
    - Bring back the method waitForTaskCompletion()
    
    Change-Id: I43696d06ee159cb84f1574aad6c785b2d6cad96e

--- photon-model-tasks/src/test/java/com/vmware/photon/controller/model/tasks/ProvisioningUtils.java
+++ photon-model-tasks/src/test/java/com/vmware/photon/controller/model/tasks/ProvisioningUtils.java
@@ -14,18 +14,23 @@
 package com.vmware.photon.controller.model.tasks;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.TimeoutException;
 
 import com.vmware.photon.controller.model.resources.ComputeService;
 import com.vmware.photon.controller.model.resources.NetworkService;
 import com.vmware.photon.controller.model.resources.NetworkService.NetworkState;
 import com.vmware.xenon.common.ServiceDocumentQueryResult;
+import com.vmware.xenon.common.TaskState;
 import com.vmware.xenon.common.UriUtils;
 import com.vmware.xenon.common.Utils;
 import com.vmware.xenon.common.test.VerificationHost;
+import com.vmware.xenon.services.common.TaskService.TaskServiceState;
 
 /**
  * Helper class for VM provisioning tests.
@@ -40,6 +45,48 @@ public class ProvisioningUtils {
                         ComputeService.FACTORY_LINK)));
         return res.documents.size() - 1;
 
+    }
+
+    public static void waitForTaskCompletion(VerificationHost host,
+                                             List<URI> provisioningTasks, Class<? extends TaskServiceState> clazz)
+            throws Throwable, InterruptedException, TimeoutException {
+        Date expiration = host.getTestExpiration();
+        List<String> pendingTasks = new ArrayList<String>();
+        do {
+            pendingTasks.clear();
+            // grab in parallel, all task state, from all running tasks
+            Map<URI, ? extends TaskServiceState> taskStates = host.getServiceState(null,
+                    clazz,
+                    provisioningTasks);
+
+            boolean isConverged = true;
+            for (Entry<URI, ? extends TaskServiceState> e : taskStates
+                    .entrySet()) {
+                TaskServiceState currentState = e.getValue();
+
+                if (currentState.taskInfo.stage == TaskState.TaskStage.FAILED) {
+                    throw new IllegalStateException(
+                            "Task failed:" + Utils.toJsonHtml(currentState));
+                }
+
+                if (currentState.taskInfo.stage != TaskState.TaskStage.FINISHED) {
+                    pendingTasks.add(currentState.documentSelfLink);
+                    isConverged = false;
+                }
+            }
+
+            if (isConverged) {
+                return;
+            }
+
+            Thread.sleep(1000);
+        } while (new Date().before(expiration));
+
+        for (String taskLink : pendingTasks) {
+            host.log("Pending task:\n%s", taskLink);
+        }
+
+        throw new TimeoutException("Some tasks never finished");
     }
 
     public static ServiceDocumentQueryResult queryComputeInstances(VerificationHost host,
