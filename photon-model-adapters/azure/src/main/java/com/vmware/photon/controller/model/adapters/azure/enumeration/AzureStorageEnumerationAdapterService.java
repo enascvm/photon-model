872c40008dd4cc0711dbc38f10579f8c4f2d7230
Sarvesh Sharma <ssarvesh@vmware.com>
2017-12-17 20:29:44 -0800

    Fix storage resource groups enumeration
     - With each enumeration storage resource groups will
       fail to identify local groups and result in deleting
       and creating new groups. Fixed the bug with changes
       in this commit.
    
    Change-Id: I9517215b4604162b467b1eaba73e1398b6d2908a

--- photon-model-adapters/azure/src/main/java/com/vmware/photon/controller/model/adapters/azure/enumeration/AzureStorageEnumerationAdapterService.java
+++ photon-model-adapters/azure/src/main/java/com/vmware/photon/controller/model/adapters/azure/enumeration/AzureStorageEnumerationAdapterService.java
@@ -477,7 +477,8 @@ public class AzureStorageEnumerationAdapterService extends StatelessService {
                     storageAccounts.size()));
 
             for (StorageAccount storageAccount : storageAccounts) {
-                String id = storageAccount.id;
+                String id = canonizeId(storageAccount.id);
+                storageAccount.id = id;
                 context.storageAccountsToUpdateCreate.put(id, storageAccount);
                 context.storageAccountIds.add(id);
                 context.storageAccountMap.put(id, storageAccount);
@@ -520,7 +521,7 @@ public class AzureStorageEnumerationAdapterService extends StatelessService {
             Query instanceIdFilter = Query.Builder
                     .create(Occurance.SHOULD_OCCUR)
                     .addFieldClause(StorageDescription.FIELD_NAME_ID,
-                            account.getValue().id)
+                            canonizeId(account.getValue().id))
                     .build();
             instanceIdFilterParentQuery.addClause(instanceIdFilter);
         }
@@ -935,10 +936,9 @@ public class AzureStorageEnumerationAdapterService extends StatelessService {
 
                             nextContainerResults = contSegment.getContinuationToken();
                             for (CloudBlobContainer container : contSegment.getResults()) {
-                                String uri = container.getUri().toString();
+                                String uri = canonizeId(container.getUri().toString());
                                 context.containerIds.add(uri);
-                                context.storageContainers.put(canonizeId(uri),
-                                        container);
+                                context.storageContainers.put(uri, container);
                                 ResultContinuation nextBlobResults = null;
                                 do {
                                     ResultSegment<ListBlobItem> blobsSegment = container
@@ -951,8 +951,7 @@ public class AzureStorageEnumerationAdapterService extends StatelessService {
 
                                     nextBlobResults = blobsSegment.getContinuationToken();
                                     for (ListBlobItem blobItem : blobsSegment.getResults()) {
-                                        String blobId = AzureUtils.canonizeId(
-                                                blobItem.getUri().toString());
+                                        String blobId = canonizeId(blobItem.getUri().toString());
                                         context.storageBlobs.put(blobId, blobItem);
                                         // populate mapping of blob uri and storage account for all storage
                                         // accounts as new disks can be added to already existing blobs
@@ -1119,7 +1118,7 @@ public class AzureStorageEnumerationAdapterService extends StatelessService {
             CloudBlobContainer container, String storageLink,
             ResourceGroupState oldResourceGroupState) {
         ResourceGroupState resourceGroupState = new ResourceGroupState();
-        resourceGroupState.id = AzureUtils.canonizeId(container.getUri().toString());
+        resourceGroupState.id = canonizeId(container.getUri().toString());
         resourceGroupState.name = container.getName();
         resourceGroupState.computeHostLink = context.parentCompute.documentSelfLink;
         if (storageLink != null) {
@@ -1249,7 +1248,7 @@ public class AzureStorageEnumerationAdapterService extends StatelessService {
         queryLocalStates.queryDocuments(rg -> {
             // if the resource group is present in Azure and has older timestamp in local
             // repository it means nothing has changed about it.
-            if (context.containerIds.contains(rg.id)) {
+            if (context.containerIds.contains(canonizeId(rg.id))) {
                 return;
             }
 
@@ -1376,8 +1375,7 @@ public class AzureStorageEnumerationAdapterService extends StatelessService {
             qBuilder.addInClause(
                     ResourceState.FIELD_NAME_ID,
                     context.storageBlobs.values().stream()
-                            .map(sb -> AzureUtils.canonizeId(QuerySpecification
-                                    .toMatchValue(sb.getUri())))
+                            .map(sb -> canonizeId(QuerySpecification.toMatchValue(sb.getUri())))
                             .collect(Collectors.toSet()));
         }
 
@@ -1628,7 +1626,7 @@ public class AzureStorageEnumerationAdapterService extends StatelessService {
         } else {
             StorageAccount storageAccount = context.storageAccountBlobUriMap
                     .get(blob.getUri().toString());
-            diskState.id = AzureUtils.canonizeId(blob.getUri().toString());
+            diskState.id = canonizeId(blob.getUri().toString());
             diskState.documentSelfLink = UUID.randomUUID().toString();
             if (storageAccount != null) {
                 diskState.regionId = storageAccount.location;
